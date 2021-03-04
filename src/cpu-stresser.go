a8032b680d08440b42b4c79cd0e0e4e54e114150
Leonardo Pinto <leoviveiros@gmail.com>
2021-03-04 11:50:12 -0300

    refatoracao cpu stresser

--- src/cpu-stresser.go
+++ src/cpu-stresser.go
@@ -1,12 +1,29 @@
 package main
 
-import "math"
+import (
+	"crypto/sha512"
+	"strings"
+)
 
+// O algorítmo do exemplo em PHP não estava estressando a CPU suficientemente, mesmo com um count maior
 func CpuStresser() {
-	x := 0.0001
+	const count = 20
+
+	builder := strings.Builder{}
+	sha := sha512.New()
+
+	builder.WriteString("aBcDeF")
+
+	for i := 0; i < count; i++ {
+		sum := sha.Sum([]byte(builder.String()))
+		builder.Write(sum)
+	}
+
+	// PHP example
+	/* x := 0.0001
 	const count = 10000000
 
 	for i := 0; i <= count; i++ {
 		x += math.Sqrt(x)
-	}
+	} */
 }
