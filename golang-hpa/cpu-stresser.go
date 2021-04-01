29ebd9666a5fa59bcfc658a655af5ca2136db682
Leonardo Pinto <leoviveiros@gmail.com>
2021-04-01 17:13:08 -0300

    algoritmo original cpu-stresser

--- golang-hpa/cpu-stresser.go
+++ golang-hpa/cpu-stresser.go
@@ -1,29 +1,12 @@
 package main
 
-import (
-	"crypto/sha512"
-	"strings"
-)
+import "math"
 
-// O algorítmo do exemplo em PHP não estava estressando a CPU suficientemente, mesmo com um count maior
 func CpuStresser() {
-	const count = 23
-
-	builder := strings.Builder{}
-	sha := sha512.New()
-
-	builder.WriteString("aBcDeFgHiJkLMnOpQrStUvWxYz")
-
-	for i := 0; i < count; i++ {
-		sum := sha.Sum([]byte(builder.String()))
-		builder.Write(sum)
-	}
-
-	// PHP example
-	/* x := 0.0001
+	x := 0.0001
 	const count = 10000000
 
 	for i := 0; i <= count; i++ {
 		x += math.Sqrt(x)
-	} */
+	}
 }
